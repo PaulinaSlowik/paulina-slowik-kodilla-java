1e78fd489a08ed8833159f61d811345261f7e50f
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-07-11 17:55:37 +0200

    First Commit

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -27,17 +27,33 @@ public class CollectionTestSuite {
     @Test
     public void testOddNumbersExterminatorNormalList(){
         //Given
-        ArrayList<Integer> even  = new ArrayList<Integer>(Arrays.asList(8,12,18,20,26,30));
-        ArrayList<Integer> odd  = new ArrayList<Integer>(Arrays.asList(8,18,30));
+        ArrayList<Integer>oddNumbers = new ArrayList<Integer>();
+        oddNumbers.add(8);
+        oddNumbers.add(12);
+        oddNumbers.add(18);
+        oddNumbers.add(20);
+        oddNumbers.add(26);
+        //ArrayList<Integer> evenAndOdd  = new ArrayList<Integer>(Arrays.asList(8,9,12,15,18,20,25,26,30));
+        //ArrayList<Integer> odd  = new ArrayList<Integer>(Arrays.asList(9,15,25,26));
         //When
-        ArrayList<Integer> listEven = OddNumbersExterminator.exterminate(even);
-        ArrayList<Integer> listOdd = OddNumbersExterminator.exterminate(odd);
-        System.out.println("Testing " + listEven);
+        ArrayList<Integer> listOdd = OddNumbersExterminator.exterminate(oddNumbers);
         System.out.println("Testing " + listOdd);
         //Then
-        assertEquals(Arrays.asList(even), Arrays.asList(listEven));
-        assertEquals(Arrays.asList(odd), Arrays.asList(listOdd));
-        //Assert.assertArrayEquals(even.size(),listEven.size());
-        //Assert.assertArrayEquals(odd.size(),listOdd.size());
+        assertEquals(Arrays.asList(oddNumbers), Arrays.asList(listOdd));
+    }
+    @Test
+    public void testEvenNumbersExterminatorNormalList(){
+        //Given
+        ArrayList<Integer>evenNumbers = new ArrayList<Integer>();
+        evenNumbers.add(9);
+        evenNumbers.add(13);
+        evenNumbers.add(19);
+        evenNumbers.add(21);
+        evenNumbers.add(27);
+        //When
+        ArrayList<Integer> listEven = OddNumbersExterminator.exterminate(evenNumbers);
+        System.out.println("Testing " + listEven);
+        //Then
+        assertEquals(Arrays.asList(evenNumbers), Arrays.asList(listEven));
     }
 }
