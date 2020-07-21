65e9da0de7a9139c0035c423ccca431ddec9df93
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-07-21 08:59:57 +0200

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -12,6 +13,8 @@ public class StreamMain {
         poemBeautifier.beautify("Text for decoration",(textForDecoration -> "Start: " +textForDecoration + "... End"));
         poemBeautifier.beautify("Text for decoration",(textForDecoration -> textForDecoration.concat(" ").concat("x2")));
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
 
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.iterate;
+//import biblioteki java.util.stream
+import java.util.stream.*;
+
+public class NumbersGenerator {
+    //metoda statyczna generateEven(int max), której zadaniem jest wygenerowanie kolejnych liczb parzystych. Metoda otrzymuje jeden argument wejściowy max mówiący o tym ile liczb "pozwalamy" wygenerować naszemu strumieniowi Stream
+    public static void generateEven(int max){
+        //wywołanie metody statycznej iterate(Integer seed, UnaryOperator f) interfejsu Stream.
+        //metoda iterate tworzy strumień obiektów typu Integer o wartości początkowej określonej przez argument seed czyli 1
+        //wartości kolejnych obiektów w strumieniu wyznaczone są przez wyrażenie lambda f (lub referencję do metody). Wyrażenie lambda otrzymuje jeden argument wejściowy (aktualną wartość obiektu w strumieniu) a jgo zadaniem jest zwrócenie kolejnej wartości (poniżej wyrażenie lambda inkrementuje arguent tzn że kolejne obiekty w strumieniu będą miały wartość o jeden większą od poprzedniego (n -> n + 1)
+        Stream.iterate(1,n -> n + 1)
+                //wywołąnie metody limit(long maxSize) interfejsu Stream która ogranicza wielkość strumienia do maxSize obiektów (opreracja filtrująca), więc po wywołąniu tego powstaje nowy Stream
+                .limit(max)
+                //na nowym Stream wywołanie metody filter (Predicate predicate) gdzie predicate jest wyrażeniem lambda które powinno zwracać true, gdy obiekt ma pozostać w strumieniu oraz wartość false gdy obiekt powinien zostać "odrzucony" i nie przekazany do strumienia wyjściowego metody filter(Predicate predicate), jest to opracja filtrująca
+                .filter(n -> n % 2 == 0)
+                //metoda forEach(Consumer action) zamyka strumień i przekształca go w końcowy typ konkretny czyli jest metodą terminalną. Działanie tej metody polega na wykonaniu dla każdego obiektu w strumieniu wyrażenia lambda- action (lub może też wykonywać referencję do metody). Typem zwracanym przez for Each (Consumer action) jest typ void (nie powstaje żadna kolekcja czy finalny zbiór danych- obiekty są niszczone). Jednak ta metoda została wykorzystana do wyświetlenia na ekranie konsoli wszystkich obiektow ze strumienia i tak też zrobiliśmy wskazując metodę System.out::println jako metodę która ma być wykonana na każdym obiekcie w strumieniu (jest to operacja terminalna0kolektor).
+                .forEach(System.out::println);
+    }
+}
