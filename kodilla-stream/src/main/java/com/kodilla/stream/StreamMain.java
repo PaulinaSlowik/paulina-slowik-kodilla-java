64cd7080daf908955df40854828d311d950813fc
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-07-22 00:10:50 +0200

    Zadanie 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,16 +4,19 @@ import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.time.temporal.ChronoUnit.*;
 
 public class StreamMain {
     public static void main(String[] args) {
         Forum theForumUser = new Forum();
 
         Map<Integer, ForumUser> theResultMapOfUsers = theForumUser.getUserList().stream()
-                .filter(user -> user.getSex().equals('M'))
-                .filter(user -> user.getDateOfBirth() > LocalDate.now()-20 )
+                .filter(user -> user.getSex()==('M'))
+                .filter(user -> user.getDateOfBirth().getYear() < (LocalDate.now().getYear())-20)
                 .filter(user -> user.getNumberOfPostPublished() > 0)
                 .collect(Collectors.toMap(ForumUser::getUniqueUserID, user ->user));
         theResultMapOfUsers.entrySet().stream()
