692c146fd88aa0769d0a4e2b1b282f14c6ac33ab
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-07-26 10:39:36 +0200

    Zadanie 7.5

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,12 +8,13 @@ public class ArrayOperationsTestSuite {
     public void testGetAverage(){
         //Given
         int[]numbers = new int[] {1,2,3,4,5,6,7};
-        double sum = 0;
-        for(int i=0; i<numbers.length; i++){
-            sum += numbers[i];
-        }
+        //double sum = 0;
+        //for(int i=0; i<numbers.length; i++){
+          //  sum += numbers[i];
+        //}
         //When
-        double expected = sum/numbers.length;
+        double expected = 4.0;
+        //double expected = sum/numbers.length;
         double actual = ArrayOperations.getAverage(numbers);
         //Then
         Assert.assertEquals(expected,actual,0.01);
