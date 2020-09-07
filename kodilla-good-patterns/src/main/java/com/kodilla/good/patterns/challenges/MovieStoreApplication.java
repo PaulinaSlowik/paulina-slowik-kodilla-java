c77c0502b8ce965c0873f137993b4bda8396b1b0
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-09-07 10:26:00 +0200

    Task 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreApplication.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+
+public class MovieStoreApplication {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        String theResultStringOfBooks = movieStore.getMovies().entrySet().stream()
+                .flatMap(book -> book.getValue().stream())
+                .collect(Collectors.joining("!"));
+        System.out.println(theResultStringOfBooks);
+    }
+}
