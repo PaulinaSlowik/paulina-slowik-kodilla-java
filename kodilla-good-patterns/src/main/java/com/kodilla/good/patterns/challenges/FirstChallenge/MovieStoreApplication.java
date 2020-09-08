7b977a1080cbc4a4c5c692ba4f97b62c4d5523c2
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-09-08 09:29:44 +0200

    challenge from module 19.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FirstChallenge/MovieStoreApplication.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FirstChallenge/MovieStoreApplication.java
@@ -2,7 +2,6 @@ package com.kodilla.good.patterns.challenges.FirstChallenge;
 
 import java.util.stream.Collectors;
 
-
 public class MovieStoreApplication {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
