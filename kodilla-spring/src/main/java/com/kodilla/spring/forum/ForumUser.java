33589fc0ce98d4c62ee007bc17156be0d155ad18
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-09-15 22:30:24 +0200

    module 14- Spring

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class ForumUser {
     String username;
 
-    public ForumUser(String username) {
+    public ForumUser() {
         this.username = "John Smith";
     }
 
--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(ForumUser.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumUserTestSuite {
     @Test
