4e90150ee4448bdd7d8750c8ab889af3b040661b
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-09-30 17:40:44 +0200

    Module 15, homework 15.4

--- kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
@@ -1,15 +1,41 @@
 package com.kodilla.patterns.factory.tasks;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+
 public class TaskFactoryTestSuite {
-//    @Test
-//    public void testShoppingTask() {
-//        //Given
-//        TaskFactory taskFactory = new TaskFactory();
-//        //When
-//        Task shoppingTask = taskFactory.makeTask(TaskFactory.SHOPPING);
-//        //Then
-//
-//    }
+    @Test
+    public void testShoppingTask() {
+        //Given
+        TaskFactory taskFactory = new TaskFactory();
+        //When
+        Task shoppingTask = taskFactory.makeTask(TaskFactory.SHOPPING);
+        shoppingTask.executeTask();
+        //Then
+        Assert.assertFalse(shoppingTask.isTaskExecuted());
+        Assert.assertEquals("Shopping Task", shoppingTask.getTaskName());
+    }
+    @Test
+    public void testPaintingTask() {
+        //Given
+        TaskFactory taskFactory = new TaskFactory();
+        //When
+        Task paintingTask = taskFactory.makeTask(TaskFactory.PAINTING);
+        paintingTask.executeTask();
+        //Then
+        Assert.assertFalse(paintingTask.isTaskExecuted());
+        Assert.assertEquals("Painting Task", paintingTask.getTaskName());
+    }
+    @Test
+    public void testDrivingingTask() {
+        //Given
+        TaskFactory taskFactory = new TaskFactory();
+        //When
+        Task drivingTask = taskFactory.makeTask(TaskFactory.DRIVING);
+        drivingTask.executeTask();
+        //Then
+        Assert.assertFalse(drivingTask.isTaskExecuted());
+        Assert.assertEquals("Driving Task", drivingTask.getTaskName());
+    }
 }
