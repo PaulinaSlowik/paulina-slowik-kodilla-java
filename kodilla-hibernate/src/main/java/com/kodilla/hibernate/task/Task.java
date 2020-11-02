accfcc9e370096f50734b6a0adf4e4ddc543bbf0
PaulinaSlowik <slowik.paulina92@gmail.com>
2020-11-02 12:18:31 +0100

    bug from kodilla tasks 17.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -6,6 +6,22 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
+
+@NamedQueries({
+        @NamedQuery(name = "Task.retrieveLongTasks",
+                query = "FROM Task WHERE duration >10"),
+        @NamedQuery(name = "Task.retrieveShortTasks",
+                query = "FROM Task WHERE duration <=10"),
+        @NamedQuery(name = "Task.retrieveTasksWithDurationLongerThan",
+                query = "FROM Task WHERE duration> :DURATION")
+})
+
+@NamedNativeQuery(
+        name = "Task.retrieveTasksWithEnoughTime",
+        query = "SELECT * FROM TASKS " +
+                "WHERE DATEDIFF(DATE_ADD(CREATED, INTERVAL DURATION DAY),NOW())>5",
+        resultClass = Task.class
+)
 @Entity
 @Table(name = "TASKS")
 public class Task {
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -1,7 +1,9 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -9,6 +11,18 @@ import java.util.List;
 
 @Transactional
 @Repository
-public interface TaskDao extends CrudRepository<Task, Integer> {
+public interface TaskDao extends CrudRepository<Task,Integer> {
     List<Task> findByDuration(int duration);
+
+    @Query
+    List<Task> retrieveLongTasks();
+
+    @Query
+    List<Task> retrieveShortTasks();
+
+    @Query(nativeQuery = true)
+    List<Task> retrieveTasksWithEnoughTime();
+
+    @Query
+    List<Task>retrieveTasksWithDurationLongerThan(@Param("DURATION") int duration);
 }
